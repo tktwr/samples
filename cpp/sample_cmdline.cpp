99349c1b8430cfab05b3ac9d3817024c10774d3f
tktwr <takehiro.tawara@gmail.com>
2018-08-04 12:46:10 +0900

    add cmdline sample

---
+++ cpp/sample_cmdline.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <string>
+#include <cmdline.h>
+
+using namespace std;
+
+int main(int argc, char *argv[]) {
+    cmdline::parser a;
+    a.add<string>("host", 'h', "host name", true, "");
+    a.add<int>("port", 'p', "port number", false, 80, cmdline::range(1, 65535));
+    a.add<string>("type", 't', "protocol type", false, "http", cmdline::oneof<string>("http", "https", "ssh", "ftp"));
+    a.add("gzip", '\0', "gzip when transfer");
+
+    a.parse_check(argc, argv);
+
+    cout << a.get<string>("type") << "://"
+        << a.get<string>("host") << ":"
+        << a.get<int>("port") << endl;
+
+    if (a.exist("gzip")) cout << "gzip" << endl;
+
+    return 0;
+}
+
