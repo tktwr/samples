2df43d35c3138059653b1ff8cb430aeb5f99177a
tktwr <takehiro.tawara@gmail.com>
2020-09-09 22:45:36 +0900

    add 2d plot

--- py/sample_matplotlib.py
+++ py/sample_matplotlib.py
@@ -6,19 +6,47 @@ import sys
 import numpy as np
 import matplotlib.pyplot as plt
 
-def main():
+
+def plot_graph():
     # 描画範囲の指定
     # x = np.arange(x軸の最小値, x軸の最大値, 刻み)
     x = np.arange(-3, 3, 0.1)
-
-    # 計算式
     y = x * x
-
-    # 横軸の変数。縦軸の変数。
     plt.plot(x, y)
-
-    # 描画実行
     plt.show()
 
-main()
 
+def read_data(fname):
+    with open(fname, 'r') as f:
+        lines = f.readlines()
+
+    x = []
+    y = []
+
+    for line in lines:
+        l = line.split(" ")
+        x.append(float(l[0]))
+        y.append(float(l[1]))
+
+    return (x, y)
+
+
+def plot_data():
+    #x = np.random.rand(100)
+    #y = np.random.rand(100)
+    x, y = read_data("data_plot.txt")
+
+    fig, ax = plt.subplots()
+    ax.plot(x, y, "o")
+    ax.set_xlabel('x axis')
+    ax.set_ylabel('y axis')
+    plt.show()
+
+
+def main(argv):
+    plot_graph()
+    plot_data()
+
+
+if __name__ == "__main__":
+    main(sys.argv)
