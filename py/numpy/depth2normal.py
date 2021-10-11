37b57ef193ab22de1d5940498612d984216813a0
tktwr <takehiro.tawara@gmail.com>
2021-10-11 21:36:39 +0900

    add depth2normal.py

---
+++ py/numpy/depth2normal.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import sys
+import cv2
+import numpy as np
+
+
+def normalize_vector(v):
+    l = np.sqrt(v[0]**2 + v[1]**2 + v[2]**2)
+    return v / l
+
+
+def depth_to_normal(ifname, ofname):
+    gray_img = cv2.imread(ifname, cv2.IMREAD_GRAYSCALE)
+    gray_img = gray_img.astype('float')
+    h, w = np.shape(gray_img)
+    nml_img = np.zeros((h, w, 3), dtype=float)
+
+    for y in range(1, h-1):
+        for x in range(1, w-1):
+            dzdx = (gray_img[y, x+1] - gray_img[y, x-1]) / 2.0
+            dzdy = (gray_img[y+1, x] - gray_img[y-1, x]) / 2.0
+            d = (-dzdx, dzdy, 1.0)
+            n = normalize_vector(d)
+            nml_img[y, x] = n * 0.5 + 0.5
+    nml_img *= 255
+    nml_img = nml_img.astype('uint8')
+    nml_img = cv2.cvtColor(nml_img, cv2.COLOR_RGB2BGR)
+    cv2.imwrite(f"{ofname}", nml_img)
+
+
+def main(argv):
+    depth_to_normal(argv[1], argv[2])
+
+
+if __name__ == "__main__":
+    main(sys.argv)
+
