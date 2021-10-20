0e8bacb0fa727e35d14bab3ccd5ae50b972b7aa7
tktwr <takehiro.tawara@gmail.com>
2021-10-20 09:52:54 +0900

    update

--- cpp/sample_c++20.cpp
+++ cpp/sample_c++20.cpp
@@ -1,9 +1,19 @@
 // *memo_cpp.20*
 #include <iostream>
+#ifdef USE_CPP20
+#include <format>
+#endif
 
 using namespace std;
 
+void f_format() {
+#ifdef USE_CPP20
+    std::string message = std::format("The answer is {}.", 42);
+#endif
+}
+
 int main(int argc, char *argv[]) {
+    f_format();
     return 0;
 }
 
