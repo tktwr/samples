6f6161cf6fd1160e65998c4128b5b2e0ab801f5d
tktwr <takehiro.tawara@gmail.com>
2022-03-19 23:35:47 +0900

    update

--- py/test_util/test_image.py
+++ py/test_util/test_image.py
@@ -8,14 +8,17 @@ import cv_util as cu
 def f_new():
     shape = (200, 300, 3)
     dtype = 'uint8'
-    val = (255, 0, 0)
-    img = cu.cv_create_img(shape, dtype, val)
+    bgr = (255, 0, 0)
+
+    img = cu.cv_create_img(shape, dtype, bgr)
+
     fname = f'new_3_{dtype}.png'
     cu.cv_save(fname, img)
 
 
 def f_bgr_to_bgra(dtype):
     img = cu.cv_load(f'new_3_{dtype}.png')
+
     img_bgra = cu.cv_bgr_to_bgra_img(img)
     cu.cv_save(f'bgra_{dtype}.png', img_bgra)
 
--- py/test_util/test_image.sh
+++ py/test_util/test_image.sh
@@ -1,41 +1,45 @@
 #!/bin/bash
 
 f_test_image_create_new() {
-  image-create.py -o new_4_uint8.png -s 300 200 -c 4 -t uint8 --bgr0 1 0 0 1
-  image-create.py -o new_3_uint8.png -s 300 200 -c 3 -t uint8 --bgr0 1 0 0
-  image-create.py -o new_1_uint8.png -s 300 200 -c 1 -t uint8 --bgr0 1
+  image-create.py -o new_4_uint8.png -s 300 200 -c 4 -y uint8 --bgr0 1 0 0 1
+  image-create.py -o new_3_uint8.png -s 300 200 -c 3 -y uint8 --bgr0 1 0 0
+  image-create.py -o new_1_uint8.png -s 300 200 -c 1 -y uint8 --bgr0 1
 
-  image-create.py -o new_4_uint16.png -s 300 200 -c 4 -t uint16 --bgr0 1 0 0 1
-  image-create.py -o new_3_uint16.png -s 300 200 -c 3 -t uint16 --bgr0 1 0 0
-  image-create.py -o new_1_uint16.png -s 300 200 -c 1 -t uint16 --bgr0 1
+  image-create.py -o new_4_uint16.png -s 300 200 -c 4 -y uint16 --bgr0 1 0 0 1
+  image-create.py -o new_3_uint16.png -s 300 200 -c 3 -y uint16 --bgr0 1 0 0
+  image-create.py -o new_1_uint16.png -s 300 200 -c 1 -y uint16 --bgr0 1
 
   # 4ch float32 exr is not supported
-  #image-create.py -o new_4_float32.exr -s 300 200 -c 4 -t float32 --bgr0 1 0 0 1
-  image-create.py -o new_3_float32.exr -s 300 200 -c 3 -t float32 --bgr0 1 0 0
-  image-create.py -o new_1_float32.exr -s 300 200 -c 1 -t float32 --bgr0 1
+  #image-create.py -o new_4_float32.exr -s 300 200 -c 4 -y float32 --bgr0 1 0 0 1
+  image-create.py -o new_3_float32.exr -s 300 200 -c 3 -y float32 --bgr0 1 0 0
+  image-create.py -o new_1_float32.exr -s 300 200 -c 1 -y float32 --bgr0 1
 }
 
 f_test_image_create_grad() {
-  image-create.py -a hgrad -o hgrad_3_uint8.png -s 300 200 -c 3 -t uint8 --bgr0 0 0 1 --bgr1 0 1 0
-  image-create.py -a vgrad -o vgrad_3_uint8.png -s 300 200 -c 3 -t uint8 --bgr0 0 0 1 --bgr1 0 1 0
+  image-create.py -a hgrad -o hgrad_3_uint8.png -s 300 200 -c 3 -y uint8 --bgr0 0 0 1 --bgr1 0 1 0
+  image-create.py -a vgrad -o vgrad_3_uint8.png -s 300 200 -c 3 -y uint8 --bgr0 0 0 1 --bgr1 0 1 0
 
-  image-create.py -a hgrad -o hgrad_3_float32.exr -s 300 200 -c 3 -t float32 --bgr0 0 0 1 --bgr1 0 1 0
-  image-create.py -a vgrad -o vgrad_3_float32.exr -s 300 200 -c 3 -t float32 --bgr0 0 0 1 --bgr1 0 1 0
+  image-create.py -a hgrad -o hgrad_3_float32.exr -s 300 200 -c 3 -y float32 --bgr0 0 0 1 --bgr1 0 1 0
+  image-create.py -a vgrad -o vgrad_3_float32.exr -s 300 200 -c 3 -y float32 --bgr0 0 0 1 --bgr1 0 1 0
 
-  image-create.py -a hgrad -o hgrad_1_float32.exr -s 300 200 -c 1 -t float32 --bgr0 0 --bgr1 1
-  image-create.py -a vgrad -o vgrad_1_float32.exr -s 300 200 -c 1 -t float32 --bgr0 0 --bgr1 1
+  image-create.py -a hgrad -o hgrad_1_float32.exr -s 300 200 -c 1 -y float32 --bgr0 0 --bgr1 1
+  image-create.py -a vgrad -o vgrad_1_float32.exr -s 300 200 -c 1 -y float32 --bgr0 0 --bgr1 1
 }
 
 f_test_image_split() {
-  image-split.py hgrad_3_uint8.png
+  image-op.py --split hgrad_3_uint8.png
 }
 
 f_test_image_merge() {
-  image-merge.py merge.png hgrad_3_uint8_0.png hgrad_3_uint8_1.png hgrad_3_uint8_2.png
+  image-op.py --merge -o merge.png hgrad_3_uint8_0.png hgrad_3_uint8_1.png hgrad_3_uint8_2.png
 }
 
 f_test_image_tile() {
   image-tile.py -c 2 hgrad_3_uint8.png vgrad_3_uint8.png vgrad_3_uint8.png hgrad_3_uint8.png
+}
+
+f_test_image_conv() {
+  image-conv.py -t 200 $MY_SAMPLES/data/sample*.jpg
 }
 
 f_test_image_create_new
@@ -43,4 +47,5 @@ f_test_image_create_grad
 f_test_image_split
 f_test_image_merge
 f_test_image_tile
+f_test_image_conv
 
