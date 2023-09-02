a02f83e65d4780d59bb882978284fc8884576d2e
tktwr <takehiro.tawara@gmail.com>
2023-09-02 19:00:28 +0900

    update

--- py/make.sh
+++ py/make.sh
@@ -7,20 +7,8 @@
 #======================================================
 # functions
 #======================================================
-f_run() {
-  print_versions.py
-  sample_argparse.py
-  sample_json.py
-  sample_matplotlib.py
-  sample_os.py
-  sample_string.py
-  sample_syntax.py
-  sample_text.py
-}
-
-f_run_ext() {
-  sample_kivy.py
-  sample_skimage.py
+f_all() {
+  ./main.py
 }
 
 f_clean_n() {
@@ -33,21 +21,7 @@ f_clean() {
   myclean.sh --pycache-dir -rm
 }
 
-#------------------------------------------------------
-f_help() {
-  echo "run"
-  echo "clean_n"
-  echo "clean"
-  echo "help"
-}
-
-f_default() {
-  f_help
-}
-
 #======================================================
 # main
 #======================================================
-func_name=${1:-"default"}
-shift
-eval "f_$func_name $@"
+f_fzf_main "$@"
