3ce8895a01e7736ce4a85d0c45ae8a1454d9f34a
tktwr <takehiro.tawara@gmail.com>
2024-10-05 21:14:03 +0900

    update

--- py/test/test_fname.py
+++ py/test/test_fname.py
@@ -4,19 +4,19 @@
 import re
 import tt_util as tu
 
-fname = tu.FileName("a/b/c/d.png")
+FN = tu.FileName("a/b/c/d.png")
 
-print(f"fname.origname() = {fname.origname()}")
-print(f"fname.dirname()  = {fname.dirname()}")
-print(f"fname.filename() = {fname.filename()}")
-print(f"fname.name()     = {fname.name()}")
-print(f"fname.ext()      = {fname.ext()}")
+print(f"FN.origname() = {FN.origname()}")
+print(f"FN.dirname()  = {FN.dirname()}")
+print(f"FN.filename() = {FN.filename()}")
+print(f"FN.name()     = {FN.name()}")
+print(f"FN.ext()      = {FN.ext()}")
 
-print(f'{tu.unix_path(tu.expand_env("$HOME"))}')
-print(f'{tu.unix_path(tu.expand_env("$MY_DATA"))}')
+print(f'{tu.path_unix(tu.expand_env("$HOME"))}')
+print(f'{tu.path_unix(tu.expand_env("$MY_DATA"))}')
 
-print(f'{tu.win_path(tu.expand_env("$HOME"))}')
-print(f'{tu.win_path(tu.expand_env("$MY_DATA"))}')
+print(f'{tu.path_windows(tu.expand_env("$HOME"))}')
+print(f'{tu.path_windows(tu.expand_env("$MY_DATA"))}')
 
 s = '/a/$MY_DATA/a/b'
 #s = '$HOME/a/b'
